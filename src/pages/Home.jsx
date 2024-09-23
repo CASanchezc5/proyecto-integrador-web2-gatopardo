60d75d9a109594915cdbdb7c750363a6875b8f10
CASanchezc5 <cascqf@gmail.com>
2024-09-23 18:37:49 -0500

    se modificó Home y Workshops, se añadio banner y carrusel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,29 @@
 import Footer from "../components/Footer";
 import Menu from "../components/Menu";
+import "./Home.css";
 function Home() {
   return (
     <div>
       <Menu />
-      <h1 className="home-title">Home</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a, nemo rerum, magni impedit atque aspernatur recusandae sapiente quas dolore et voluptatum dolorem aut velit architecto deserunt magnam? Animi, saepe!</p>
+      {/* <h1 className="home-title">Home</h1> */}
+      <div className="banner">
+        <img src="public/banner.jpg" alt="" />
+      </div>
+
+    
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a,
+        nemo rerum, magni impedit atque aspernatur recusandae sapiente quas
+        dolore et voluptatum dolorem aut velit architecto deserunt magnam?
+        Animi, saepe!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur a,
+        nemo rerum, magni impedit atque aspernatur recusandae sapiente quas
+        dolore et voluptatum dolorem aut velit architecto deserunt magnam?
+        Animi, saepe!
+      </p>
       <Footer />
     </div>
   );
--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -11,6 +11,40 @@ function Workshops() {
       {/* <h1 className="workshops-title">Talleres</h1> */}
       <img className="img-wokshops" src="public/logito-gato-pardo-14.png" alt="" />
       {/* <img className="img-wokshops" src="public/logito-gato-pardo-03.png" alt="" /> */}
+
+      <div class="slider-box">
+        <ul>
+          <li>
+            <img src="public/slider1.jpg" alt="" />
+            <div class="texto">
+              {/* <h2>Imagen 1</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
+            </div>
+          </li>
+          <li>
+            <img src="public/slider2.jpg" alt="" />
+            <div class="texto">
+              {/* <h2>Imagen 2</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
+            </div>
+          </li>
+          <li>
+            <img src="public/slider3.jpg" alt="" />
+            <div class="texto">
+              {/* <h2>Imagen 3</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
+            </div>
+          </li>
+          <li>
+            <img src="public/slider4.jpg" alt="" />
+            <div class="texto">
+              {/* <h2>Imagen 4</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <div className="container-cards">
         <Card /> 
         <Card /> 
