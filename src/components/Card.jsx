f5b72fac0160bd7f1f6f940dccf9dbbf41fc6adc
CASanchezc5 <cascqf@gmail.com>
2024-10-01 00:06:01 -0500

    se modifico el archivo Workshops.jsx y Card.jsx, para hacer dinamicas las cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,14 @@
 import "./Card.css";
 
-const Card = () => {
+const Card = ({title, description, image}) => {
   return (
     <div className="container-card">
       <figure className="figure-card">
-        <img className="img-card" src="public/_MG_5633.jpg" alt="img" />
+        <img className="img-card" src={image} alt={title} />
       </figure>
       <p className="paragraph-card">
-        <span className="span-card-title">Subtitulo taller</span>
-        <span className="span-card-description">
-          Descripcion del taller Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Velit ipsum ullam iure vero omnis fugit eos
-          molestiae possimus ex ipsa, nesciunt quibusdam architecto dolorem!
-          Nesciunt labore itaque possimus expedita vero. molestiae possimus ex ipsa, nesciunt quibusdam architecto dolorem! Nesciunt labore itaque possimus expedita vero.
-        </span>
+        <span className="span-card-title">{title}</span>
+        <span className="span-card-description">{description}</span>
       </p>
       {/* <div>
         <input type="button" value="Ver más" />
--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -1,58 +1,100 @@
-import Card from "../components/Card"
-import Footer from "../components/Footer"
-import Menu from "../components/Menu"
-import "./Workshops.css"
-
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Menu from "../components/Menu";
+import "./Workshops.css";
 
 function Workshops() {
+  const workshops = [
+    {
+      id: 1,
+      image: "public/_MG_5633.jpg",
+      title: "Taller 1",
+      description: "Descripción del taller 1",
+    },
+    {
+      id: 2,
+      image: "public/_MG_5633.jpg",
+      title: "Taller 2",
+      description: "Descripción del taller 2",
+    },
+    {
+      id: 3,
+      image: "public/_MG_5633.jpg",
+      title: "Taller 3",
+      description: "Descripción del taller 3",
+    },
+    {
+      id: 4,
+      image: "public/_MG_5633.jpg",
+      title: "Taller 4",
+      description: "Descripción del taller 4",
+    },
+  ];
   return (
     <div className="container">
       <Menu />
       {/* <h1 className="workshops-title">Talleres</h1> */}
       {/* <img className="img-wokshops" src="public/logito-gato-pardo-14.png" alt="" /> */}
-      
 
       <div class="slider-box">
         <ul>
           <li>
             <img src="public/slider1.jpg" alt="" />
-            <div class="texto">
-              {/* <h2>Imagen 1</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
-            </div>
+            {/* <div class="texto">
+              <h2>Imagen 1</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci quaerat ducimus odit voluptate.{" "}
+              </p>
+            </div> */}
           </li>
           <li>
             <img src="public/slider2.jpg" alt="" />
-            <div class="texto">
-              {/* <h2>Imagen 2</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
-            </div>
+            {/* <div class="texto">
+              <h2>Imagen 2</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci quaerat ducimus odit voluptate.{" "}
+              </p>
+            </div> */}
           </li>
           <li>
             <img src="public/slider3.jpg" alt="" />
-            <div class="texto">
-              {/* <h2>Imagen 3</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
-            </div>
+            {/* <div class="texto">
+              <h2>Imagen 3</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci quaerat ducimus odit voluptate.{" "}
+              </p>
+            </div> */}
           </li>
           <li>
             <img src="public/slider4.jpg" alt="" />
-            <div class="texto">
-              {/* <h2>Imagen 4</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quaerat ducimus odit voluptate. </p> */}
-            </div>
+            {/* <div class="texto">
+              <h2>Imagen 4</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci quaerat ducimus odit voluptate.{" "}
+              </p>
+            </div> */}
           </li>
         </ul>
       </div>
 
       <div className="container-cards">
-        <Card /> 
-        <Card /> 
+        {workshops.map((workshop, id) => (
+          <Card
+            key={id}
+            image={workshop.image}
+            title={workshop.title}
+            description={workshop.description}
+          />
+        ))}
       </div>
 
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Workshops
+export default Workshops;
