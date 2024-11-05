99f25093ddd79f95de0e30b37a71d66d2486da65
CASanchezc5 <cascqf@gmail.com>
2024-11-05 18:00:24 -0500

    se agrego carpeta dashboard, con sus respectivos archivos y rutas

--- src/routes/routerApp.jsx
+++ src/routes/routerApp.jsx
@@ -5,6 +5,9 @@ import Workshops from "../pages/Workshops";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import Gallery from "../pages/Gallery";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Profile from "../pages/dashboard/Profile";
+import Analytics from "../pages/dashboard/Analytics";
 
 export let routes = [
   {
@@ -37,4 +40,18 @@ export let routes = [
     element: <Register />,
     
   },
+  {
+    path: '/dashboard/',
+    element: <Dashboard />,
+    children: [
+        {
+            path: 'profile',
+            element: <Profile />
+        },
+        {
+            path: 'analytics',
+            element: <Analytics />
+        },
+    ]
+  }
 ]
